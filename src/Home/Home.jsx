f1935876193da0ccdffe035b5ebe5d42a98927a3
surbhirajpal <surbhirajpal88@gmail.com>
2023-02-06 10:21:01 +0530

    sevice section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Introduction from './components/Introduction';
+import Service from './components/Service';
 
 // import Intro from './components/Intro';
 // import About from './components/About';
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <>
             <Introduction />
+            <Service/>
         </>
     );
 };
---
+++ src/Home/components/Service/Service.jsx
@@ -0,0 +1,93 @@
+import {
+    useRef,
+    useState,
+    useLayoutEffect,
+    useEffect
+} from "react";
+
+import {
+    motion,
+    useScroll,
+    useTransform,
+} from "framer-motion";
+
+import './Service.less';
+
+const Service = () => {
+    const serviceRef = useRef(null);
+    const [scrollStart, setScrollStart] = useState(0);
+
+    const { scrollY } = useScroll();
+
+    const coderScale = useTransform(scrollY, [scrollStart, scrollStart + 200], [0.4, 0.7]);
+
+    const codingPosition = useTransform(scrollY, [scrollStart + 350, scrollStart + 750], ['-70%', '-50%']);
+    const codingOpacity = useTransform(scrollY, [scrollStart + 350, scrollStart + 750], [0, 1]);
+
+    const contentOpacity = useTransform(scrollY, [scrollStart + 350, scrollStart + 750], [0, 1]);
+
+    useEffect(() => {
+        const rect = serviceRef?.current?.getBoundingClientRect();
+        setScrollStart(rect?.top);
+    }, [serviceRef]);
+
+    console.log('scrollStart', scrollStart)
+    return (
+        <section className='service' ref={serviceRef}>
+            <div className='service__wrapper'>
+                <h1 className='service__title'>
+                    {'What I Can Do'}
+                </h1>
+                <div className='service__content-wrapper'>
+                    <div className='service__content-holder'>
+                        <motion.div
+                            style={{
+                                opacity: contentOpacity
+                            }}
+                        >
+                            <div className='service__subtitle'>
+                                <p>{'FRONTEND DEVELOPER'}</p>
+                            </div>
+                            <div className='service__content'>
+                                <p>
+                                    {`As a front-end developer, I specialize in creating and implementing the visual and interactive elements of websites and web applications. 
+                            My expertise lies in React.js and responsive design, and I have a strong understanding of the latest web development trends 
+                            and technologies.`}
+                                </p>
+                                <p>
+                                    {`Whether you need a new website, a redesign, or just a touch of magic, I am ready to bring my skills and creativity to your project. 
+                            Let's work together to create something amazing!`}
+                                </p>
+                            </div>
+                        </motion.div>
+                    </div>
+                    <div className='service__image-holder'>
+                        <div className='service__coder'>
+                            <motion.div
+                                style={{
+                                    scale: coderScale,
+                                    y: '-50%',
+                                }}
+                            >
+                                <img src={'images/service/code.svg'} />
+                            </motion.div>
+                        </div>
+                        <div className='service__coding'>
+                            <motion.div
+                                style={{
+                                    scale: 0.8,
+                                    y: codingPosition,
+                                    opacity: contentOpacity,
+                                }}
+                            >
+                                <img src={'images/service/coding.png'} />
+                            </motion.div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Service;
